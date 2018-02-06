3132cab8bea7cef67a1bbc0c9bb66438f7dac97d
Grace Carter <graciec@gmail.com>
2018-02-06 16:00:21 -0700

    escape and comments

---
+++ Labs/EscapeSequencesComments.java
@@ -0,0 +1,19 @@
+public class EscapeSequencesComments {
+    public static void main( String[] args ) {
+      // this exercise demonstrates escape sequences and EscapeSequencesComments
+      System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
+      System.out.print( "\tLean Java the \"Hard\" Way!\n");
+        //System.out.frimp( "Learn Java the Hard Way");
+      System.out.print( "Hello\n" ); //this line prints Hello.
+      System.out.print( "Jello\by\n");  //this line prints Jelly.
+      /* The quick brown fox jumped over the lazy dog.
+      quick wafting zephyrs vex bold Jim */
+      System.out /* testing */ .println( "Hard to believe, eh!" );
+      System.out.println( "Surprised? /* abcde */ Or what did you expect?");
+      System.out.println( " \\// -=- \\//" );
+      System.out.println( "\\\\ \\\\\\ \\\\\\\\"); // it takes 2 to make 1
+      System.out.println( "I hope you understand \"escape sequences\" now.\n");
+      // and comments. :)
+
+    }
+}
