80d62e0383c6b3d32e9c383c487a4475a3feae80
Grace Carter <graciec@gmail.com>
2018-02-06 16:16:30 -0700

    Prework objective 1 lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+  System.out.println("Name: Grace Carter");
+  System.out.println("Age: fortysomething");
+  System.out.println("State: TX");
+
+  }
+}
