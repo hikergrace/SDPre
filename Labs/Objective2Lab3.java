2a9c26bce0a66cbb6559b6608fcc020bbec3ffb7
Grace Carter <graciec@gmail.com>
2018-02-07 16:34:48 -0700

    Prework Objective 2 Lab 3 creating and using vars

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    String food1, food2, food3;
+    food1 = "Lasagna";
+    food2 = "Pie";
+    food3 = "Peaches";
+    System.out.println("My favorite foods are: ");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+
+  }
+}
