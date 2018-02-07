499712c0ffd4602ae2ad31d45808928658c10242
Grace Carter <graciec@gmail.com>
2018-02-07 15:54:10 -0700

    study drill for ex. 5 LJTHW

---
+++ Labs/CreatingVariables.java
@@ -0,0 +1,37 @@
+public class CreatingVariables {
+  public static void main(String[] args){
+    int x, y, age, temp;
+    double seconds, e, checking, fuel;
+    String firstName, lastName, title, storeName, tempFar;
+
+    x = 10;
+    y = 400;
+    age = 39;
+    temp = 45;
+
+    seconds = 4.71;
+    e = 2.71828182845904512536;
+    checking = 1.89;
+    fuel = 2.6704;
+
+
+    firstName = "Grace";
+    lastName = "Carter";
+    title = "Ms.";
+    tempFar = "F";
+    storeName = "Circle K";
+
+
+
+    System.out.println("The variable x contains " + x);
+    System.out.println("The value " + y + " is stored in the variable y.");
+    System.out.println("The experiment took " + seconds + " seconds.");
+    System.out.println("A favorite irrational # is Euler's number: " + e);
+    System.out.println("Hopefully you have more than $" + checking + "!");
+    System.out.println("My name's " + title + " " + firstName + " " + lastName);
+    System.out.println("Today it is " + temp + tempFar + ".");
+    System.out.println("I bought fuel at " + storeName + " " + "for " + fuel + " " + "per gallon.");
+
+  }
+
+}
