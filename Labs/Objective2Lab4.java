c3a24c4388b96e46ee78560bb5c22b25c32274a0
Grace Carter <graciec@gmail.com>
2018-02-07 16:40:38 -0700

    Prework Objective 2 Lab 4 writing expressions

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,10 @@
+public class Objective2Lab4 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 2;
+    num2 = 4;
+    num3 = 9;
+    average = (num1+num2+num3)/3;
+    System.out.println("The average of 2, 4, and 9 is: " + average);
+  }
+}
