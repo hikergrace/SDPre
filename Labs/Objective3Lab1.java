09e5ebe231455510ff1d6d312bb87a2f04a27e5e
Grace Carter <graciec@gmail.com>
2018-02-08 16:19:58 -0700

    Prework objective 3 lab 1 using scanner

--- Labs/Objective3Lab1.java
+++ Labs/Objective3Lab1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Objective3Lab1 {
   public static void main(String[] args){
-      String userInput;
+      String userInput = "";
       Scanner input = new Scanner(System.in);
 
       System.out.println("Please type in a word: ");
