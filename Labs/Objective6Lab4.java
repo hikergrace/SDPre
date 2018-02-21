b46aec41b262e71a4a91e3b209a7f7d610b5af74
Grace Carter <graciec@gmail.com>
2018-02-21 13:20:10 -0700

    Prework objective 4 lab 4 sum numbers

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args){
+    int count = 1, sum=0;
+
+    while(count <= 20){
+      count = count + 1;
+      sum = sum + count;
+  }
+
+    System.out.println("the sum is: " + sum);
+  }
+}
